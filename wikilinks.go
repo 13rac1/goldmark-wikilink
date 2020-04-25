02170b7cd7f0224839bbf3684c4700b0c4f549d9
Kevin Dangoor <kevindangoor@khanacademy.org>
2020-04-25 15:29:13 -0400

    Get rid of spurious logging
    There were some log statements that are no longer needed.

--- wikilinks.go
+++ wikilinks.go
@@ -6,7 +6,6 @@ import (
 	"github.com/yuin/goldmark/parser"
 	"github.com/yuin/goldmark/text"
 	"github.com/yuin/goldmark/util"
-	"log"
 )
 
 type FilenameNormalizer interface {
@@ -29,7 +28,6 @@ func NewWikilinksParser() *wikilinksParser {
 }
 
 func (wl *wikilinksParser) WithNormalizer(fn FilenameNormalizer) *wikilinksParser {
-	log.Println("Normalizer Set")
 	wl.normalizer = fn
 	return wl
 }
@@ -70,7 +68,6 @@ func (wl *wikilinksParser) Parse(parent ast.Node, block text.Reader, pc parser.C
 	destination := block.Value(text.NewSegment(segment.Start+2, segment.Start+pos-1))
 	destText := string(destination)
 	if wl.normalizer != nil {
-		log.Println("Calling normalizer for " + destText)
 		destText = wl.normalizer.Normalize(destText)
 	} else {
 		destText += ".html"
